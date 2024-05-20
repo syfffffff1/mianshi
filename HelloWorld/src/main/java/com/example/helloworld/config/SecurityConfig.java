0294d81205143fe69c14fcf7db2d76018f4d5d4f
15068177295 <962249586@qq.com>
2024-05-20 09:28:36 +0800

    后端面试代码

---
+++ HelloWorld/src/main/java/com/example/helloworld/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.helloworld.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        User.UserBuilder users = User.builder().passwordEncoder(passwordEncoder::encode);
+        UserDetailsService userDetailsService = new InMemoryUserDetailsManager(
+                users.username("test").password("123456").roles("USER").build()
+        );
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/hello").authenticated()
+                        .anyRequest().permitAll()
+
+                )
+                .httpBasic();
+        return http.build();
+    }
+}
+
---
+++ HelloWorld/src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+}
